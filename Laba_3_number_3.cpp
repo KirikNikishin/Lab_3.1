dc1db733ff8104d12c560b41980e7f8f16984850
KirikNikishin <58213759+KirikNikishin@users.noreply.github.com>
2022-12-04 03:10:13 +0300

    Add files via upload

--- Laba_3_number_3.cpp
+++ Laba_3_number_3.cpp
@@ -6,44 +6,44 @@
 #include <iostream>
 #include <string>
 #include <vector>
-using namespace std;
+//using namespace std;
 
-vector<int> Searching(std::sregex_iterator words_begin, std::sregex_iterator words_end) {
-
-    std::vector<int> count_arr;
-    for (std::sregex_iterator i = words_begin; i != words_end; ++i) {
-        std::smatch match = *i;
-        std::string match_str = match.str();
-        int size_ = sizeof(match_str) / sizeof(match_str[0]);
-        int power = 0;
-        int count = 0;
-        //std::cout << match_str << std::endl;
-        for (int i = match_str.length() - 1; i >= 0; i--) {
-            if (match_str[i] == '1') {
-                count += pow(2, power);
-            }
-            power += 1;
-        }
-        if (count != 0 and match_str != "0" and count % 3==0) {
-            count_arr.push_back(count);
-        }
-    }
-    return count_arr;
-}
+//std::vector<int> Searching(std::sregex_iterator words_begin, std::sregex_iterator words_end) {
+//
+//    std::vector<int> count_arr;
+//    for (std::sregex_iterator i = words_begin; i != words_end; ++i) {
+//        std::smatch match = *i;
+//        std::string match_str = match.str();
+//        int size_ = sizeof(match_str) / sizeof(match_str[0]);
+//        int power = 0;
+//        int count = 0;
+//        //std::cout << match_str << std::endl;
+//        for (int i = match_str.length() - 1; i >= 0; i--) {
+//            if (match_str[i] == '1') {
+//                count += pow(2, power);
+//            }
+//            power += 1;
+//        }
+//        if (count != 0 and match_str != "0" and count % 3==0) {
+//            count_arr.push_back(count);
+//        }
+//    }
+//    return count_arr;
+//}
 int main()
 {
-    std::string s[7] = { "11110_1111120202002_11011", "dhsjkzxzhjxj11110zcnjnk11", "Lorem 111 Ipsum", "1001 nights", "14-th century and 100001 deaths", "<html><head><title>Bank</title><head><body><button type = \"button\ class = \"button\">+1111</button><button type = \"button\ class = \"button\">110</button></body></html>", "11011_158_12_18_12B_11111_11"}; //"11110_1111120202002_11011";
-    std::regex words_regex("[0-1]+");
-    for (int i = 0; i < sizeof(s)/sizeof(s[0]); i++) {
+    std::string s[6] = { "158", "Lorem 111 Ipsum", "1001 nights", "100001", "10111", "10101011"};
+    std::regex words_regex("0*(1(00)*10*|10(00)*1(00)*(11)*0(00)*10*)*0*");
+    for (int i = 0; i < sizeof(s) / sizeof(s[0]); i++) {
         std::sregex_iterator words_begin =
             std::sregex_iterator(s[i].begin(), s[i].end(), words_regex);
         std::sregex_iterator words_end = std::sregex_iterator();
-        vector <int> arr = Searching(words_begin, words_end);
-        if (arr.size() != 0) {
-            cout << s[i] << endl;
+        std::smatch match;
+        if (std::regex_search(s[i], match, words_regex))
+        {
+            std::cout << match[0] << std::endl;
         }
     }
-    
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
